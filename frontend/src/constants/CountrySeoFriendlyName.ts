c22acc355950c90a527a79392c3f3596f9d10ba5
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-25 00:13:30 +0300

    fix: type fix 2

--- frontend/src/constants/CountrySeoFriendlyName.ts
+++ frontend/src/constants/CountrySeoFriendlyName.ts
@@ -1,6 +1,6 @@
 import { TFunction } from 'i18next';
 import { Country } from './Country';
 
-export const getSeoFriendlyName = (t: TFunction, country: Country): string => {
+export const getSeoFriendlyName = (t: TFunction<['seo'], 'seo'>, country: Country): string => {
   return t(`seo:CountrySeoFriendlyName.${country}` as const);
 };
