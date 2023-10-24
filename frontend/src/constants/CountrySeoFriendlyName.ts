6bd447ba7e8970b583683b92a559f74b2c256c71
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-25 00:11:10 +0300

    fix: type

--- frontend/src/constants/CountrySeoFriendlyName.ts
+++ frontend/src/constants/CountrySeoFriendlyName.ts
@@ -1,6 +1,6 @@
 import { TFunction } from 'i18next';
 import { Country } from './Country';
 
-export const getSeoFriendlyName = (t: TFunction<['seo'], undefined, 'seo'>, country: Country): string => {
+export const getSeoFriendlyName = (t: TFunction, country: Country): string => {
   return t(`seo:CountrySeoFriendlyName.${country}` as const);
 };
