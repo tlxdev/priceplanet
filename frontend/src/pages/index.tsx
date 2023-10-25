3229983d9b9f182f3a4ddf1c58193c1805c3b1e1
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-26 00:05:46 +0300

    fix: countryname tolowercase

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -100,7 +100,7 @@ const Lander = ({ location }: { location: GeoLocation }) => {
 
             <LanderAutoComplete />
 
-            <Link href={`/country/${countryName}`}>
+            <Link href={`/country/${countryName.toLowerCase()}`}>
               <button className="btn btn-primary px-8 mt-6 h-12 text-lg border-gray-400">
                 <div className="space-x-2">
                   <span>{t('lander:SubmitMyDetails')}</span>
