9b2eb0f222a132e725bdfd20a250c7052815c5c9
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-26 23:03:52 +0300

    fix: sitemap exclude

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://priceplanet.org',
+  exclude: ['/country/*/add-details/done'], // <= exclude here
   generateRobotsTxt: false, // (optional)
   alternateRefs: [
     {
