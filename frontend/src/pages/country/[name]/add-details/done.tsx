e52b41ed5fdb7084fa10418522e9c476f74513b2
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-26 23:07:11 +0300

    fix: done page

--- frontend/src/pages/country/[name]/add-details/done.tsx
+++ frontend/src/pages/country/[name]/add-details/done.tsx
@@ -1,10 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { Country } from '@/constants/Country';
-import { getSeoFriendlyName } from '@/constants/CountrySeoFriendlyName';
-import i18n from 'i18next';
-import Backend from 'i18next-fs-backend';
 
 const Done = () => {
   const { t } = useTranslation(['country-details-form']);
@@ -20,42 +16,14 @@ const Done = () => {
   );
 };
 
-export async function getStaticPaths() {
-  const paths = [];
-
-  const languages = ['en'];
-  const defaultLanguage = 'en';
-
-  await i18n.use(Backend).init({
-    lng: defaultLanguage,
-    fallbackLng: defaultLanguage,
-    ns: ['common'],
-    defaultNS: 'common',
-    backend: {
-      loadPath: './public/locales/{{lng}}/{{ns}}.json',
-    },
-  });
-
-  const countryPaths = Object.values(Country).map((country) => {
-    return {
-      params: { name: getSeoFriendlyName(i18n.t, country) },
-      locale: defaultLanguage,
-    };
-  });
-
-  return {
-    paths: countryPaths,
-    fallback: false,
-  };
-}
-
-// Convert next line to arrow function:
-export async function getStaticProps({ locale, params }: { locale: string; params: { name: string } }) {
+const getServerSideProps = async ({ locale, params }: { locale: string; params: { name: string } }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common', 'country-details-form'])),
+      country: params.name.toUpperCase(),
     },
   };
-}
+};
 
+export { getServerSideProps };
 export default Done;
