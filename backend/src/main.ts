53efc7e4f6b8fa7ab38cc32dcfcadd070f0996dd
Taneli Lahtela <13731577+tlxdev@users.noreply.github.com>
2023-10-26 21:20:25 +0300

    fix: required env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!process.env.CORS_ORIGIN) {
+    throw new Error('CORS_ORIGIN env variable not set');
+  }
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
   });
